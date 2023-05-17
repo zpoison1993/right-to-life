1667dbbc99251a146ded3bb683345c221de75514
vgolub <zpoison1993@gmail.com>
2023-05-17 14:33:59 +0200

    removed double quotes

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { HOME_OG_IMAGE_URL } from '../lib/constants'
+import {HOME_OG_IMAGE_URL} from '../lib/constants'
 
 const Meta = () => {
     return (
@@ -34,7 +34,7 @@ const Meta = () => {
             <link rel="alternate" type="application/rss+xml" href="/feed.xml"/>
             <meta
                 name="description"
-                content={`"\"Право на жизнь\" - это защита прав детей, подростков и незащищённых слоёв населения, популяризация правовой грамотности, пропаганда здорового образа жизни, а также развитие добровольчества и благотворительности."`}
+                content={`\"Право на жизнь\" - это защита прав детей, подростков и незащищённых слоёв населения, популяризация правовой грамотности, пропаганда здорового образа жизни, а также развитие добровольчества и благотворительности."`}
             />
             <meta property="og:image" content={HOME_OG_IMAGE_URL}/>
         </Head>
